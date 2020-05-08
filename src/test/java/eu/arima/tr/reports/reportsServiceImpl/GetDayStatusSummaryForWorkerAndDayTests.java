72d013caa7a9d3452a862b619cdfe7aeec5c3417
jessica <jessica@arima.eu>
2020-05-08 15:47:34 +0200

    Merge remote-tracking branch 'origin/feature/03_tests_improving_quality'

--- src/test/java/eu/arima/tr/reports/reportsServiceImpl/GetDayStatusSummaryForWorkerAndDayTests.java
+++ src/test/java/eu/arima/tr/reports/reportsServiceImpl/GetDayStatusSummaryForWorkerAndDayTests.java
@@ -19,55 +19,128 @@ import eu.arima.tr.reports.ReportsServiceImpl;
 import eu.arima.tr.workLogs.Worklog;
 import eu.arima.tr.workLogs.WorklogRepository;
 
-
 public class GetDayStatusSummaryForWorkerAndDayTests {
-	
+
 	@InjectMocks
 	ReportsServiceImpl reportsService;
-	
+
 	@Mock
 	WorklogRepository worklogRepository;
-	
+
+	@Mock
+	Worklog oneHourWorklog;
+	@Mock
+	Worklog twoHoursWorklog;
+	@Mock
+	Worklog fiveHoursWorklog;
+	@Mock
+	Worklog eightHoursWorklog;
+	@Mock
+	Worklog tenHoursWorklog;
+
 	@BeforeEach
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
+
+		Mockito.when(oneHourWorklog.getDuration()).thenReturn(1);
+		Mockito.when(twoHoursWorklog.getDuration()).thenReturn(2);
+		Mockito.when(fiveHoursWorklog.getDuration()).thenReturn(5);
+		Mockito.when(eightHoursWorklog.getDuration()).thenReturn(8);
+		Mockito.when(tenHoursWorklog.getDuration()).thenReturn(10);
 	}
-	
+
 	@Test
 	public void if_the_worklog_for_the_resquested_day_is_less_than_8_hours_the_status_is_MISSING_HOURS() {
+		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(),
+				ArgumentMatchers.any(LocalDate.class))).thenReturn(Arrays.asList(fiveHoursWorklog));
+
+		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
+
+		assertStatusEquals(DayStatus.MISSING_HOURS, resultado);
+
+	}
+
+	@Test
+	public void if_the_sum_of_worklogs_for_the_resquested_day_is_less_than_8_hours_the_status_is_MISSING_HOURS() {
 		Worklog worklog = Mockito.mock(Worklog.class);
 		Mockito.when(worklog.getDuration()).thenReturn(7);
-		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(), ArgumentMatchers.any(LocalDate.class))).thenReturn(Arrays.asList(worklog));
-				
+		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(),
+				ArgumentMatchers.any(LocalDate.class))).thenReturn(Arrays.asList(fiveHoursWorklog, oneHourWorklog));
+
 		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
-		
-		assertEquals(DayStatus.MISSING_HOURS, resultado.getStatusList().get(0));
-		
+
+		assertStatusEquals(DayStatus.MISSING_HOURS, resultado);
+
 	}
-	
+
 	@Test
 	public void if_the_worklog_for_the_resquested_day_is_equal_to_8_hours_the_status_is_RIGHT_HOURS() {
-		Worklog worklog = Mockito.mock(Worklog.class);
-		Mockito.when(worklog.getDuration()).thenReturn(8);
-		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(), ArgumentMatchers.any(LocalDate.class))).thenReturn(Arrays.asList(worklog));
-				
+		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(),
+				ArgumentMatchers.any(LocalDate.class))).thenReturn(Arrays.asList(eightHoursWorklog));
+
 		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
-		
-		assertEquals(DayStatus.RIGHT_HOURS, resultado.getStatusList().get(0));
-		
+
+		assertStatusEquals(DayStatus.RIGHT_HOURS, resultado);
+
 	}
-	
+
+	@Test
+	public void if_the_sum_of_worklogs_for_the_resquested_day_is_equal_to_8_hours_the_status_is_RIGHT_HOURS() {
+		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(),
+				ArgumentMatchers.any(LocalDate.class)))
+				.thenReturn(Arrays.asList(fiveHoursWorklog, oneHourWorklog, twoHoursWorklog));
+
+		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
+
+		assertStatusEquals(DayStatus.RIGHT_HOURS, resultado);
+
+	}
+
 	@Test
 	public void if_the_worklog_for_the_resquested_day_is_more_than_8_hours_the_status_is_EXTRA_HOURS() {
 		Worklog worklog = Mockito.mock(Worklog.class);
 		Mockito.when(worklog.getDuration()).thenReturn(10);
-		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(), ArgumentMatchers.any(LocalDate.class))).thenReturn(Arrays.asList(worklog));
-				
-		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
-		
-		assertEquals(DayStatus.EXTRA_HOURS, resultado.getStatusList().get(0));
-		
-	}
-	
+		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(),
+				ArgumentMatchers.any(LocalDate.class))).thenReturn(Arrays.asList(tenHoursWorklog));
 
+		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
+
+		assertStatusEquals(DayStatus.EXTRA_HOURS, resultado);
+
+	}
+
+	@Test
+	public void if_the_sum_of_worklogs_for_the_resquested_day_is_more_than_8_hours_the_status_is_EXTRA_HOURS() {
+		Worklog worklog = Mockito.mock(Worklog.class);
+		Mockito.when(worklog.getDuration()).thenReturn(10);
+		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(),
+				ArgumentMatchers.any(LocalDate.class)))
+				.thenReturn(Arrays.asList(fiveHoursWorklog, fiveHoursWorklog, oneHourWorklog));
+
+		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
+
+		assertStatusEquals(DayStatus.EXTRA_HOURS, resultado);
+	}
+
+	@Test
+	public void the_status_result_belongs_to_the_requested_worker() {
+		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
+
+		assertEquals("USU", resultado.getWorkerUserName());
+
+	}
+
+	@Test
+	public void the_status_result_belongs_to_the_requested_day() {
+		LocalDate day = LocalDate.now();
+		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", day);
+
+		assertEquals(day, resultado.getDate());
+
+	}
+
+	private void assertStatusEquals(DayStatus dayStatus, DayStatusSummary resultado) {
+		assertEquals(1, resultado.getStatusList().size());
+		assertEquals(dayStatus, resultado.getStatusList().get(0));
+	}
 }
