44abdad139240aac454f8dbcb8a99176e9218aa4
jessica <jessica@arima.eu>
2020-05-08 15:26:20 +0200

    update test class name

--- src/test/java/eu/arima/tr/reports/reportsServiceImpl/GetDayStatusSummaryForWorkerAndDayTests.java
+++ src/test/java/eu/arima/tr/reports/reportsServiceImpl/GetDayStatusSummaryForWorkerAndDayTests.java
@@ -1,11 +1,9 @@
 package eu.arima.tr.reports.reportsServiceImpl;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,41 +13,61 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import eu.arima.tr.reports.DayStatus;
+import eu.arima.tr.reports.DayStatusSummary;
 import eu.arima.tr.reports.ReportsServiceImpl;
 import eu.arima.tr.workLogs.Worklog;
 import eu.arima.tr.workLogs.WorklogRepository;
 
-public class GetDayStatusSummaryForWorkerAndDayTests {
 
+public class GetDayStatusSummaryForWorkerAndDayTests {
+	
 	@InjectMocks
 	ReportsServiceImpl reportsService;
-
+	
 	@Mock
 	WorklogRepository worklogRepository;
-
+	
 	@BeforeEach
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}
-
+	
 	@Test
-	public void get_status_summary_for_worker_and_day() {
-		reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
-		assertTrue(true);
-	}
-
-	@Test
-	public void calculates_the_status_based_on_worker_and_date_worklogs() {
-		List<Worklog> partes = new ArrayList<Worklog>();
-		Worklog wl = new Worklog();
-		wl.setFromTime(LocalTime.of(8,0,0));
-		wl.setToTime(LocalTime.of(19,0,0));
-		partes.add(wl);
-		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(), ArgumentMatchers.any(LocalDate.class))).thenReturn(partes); 
+	public void if_the_worklog_for_the_resquested_day_is_less_than_8_hours_the_status_is_MISSING_HOURS() {
+		Worklog worklog = Mockito.mock(Worklog.class);
+		Mockito.when(worklog.getDuration()).thenReturn(7);
+		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(), ArgumentMatchers.any(LocalDate.class))).thenReturn(Arrays.asList(worklog));
+				
+		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
 		
-		LocalDate fecha = LocalDate.now();
-		reportsService.getDayStatusSummaryForWorkerAndDay("USU", fecha);
-
-		Mockito.verify(worklogRepository).findByUsernameAndDate("USU", fecha);
+		assertEquals(DayStatus.MISSING_HOURS, resultado.getStatusList().get(0));
+		
 	}
+	
+	@Test
+	public void if_the_worklog_for_the_resquested_day_is_equal_to_8_hours_the_status_is_RIGHT_HOURS() {
+		Worklog worklog = Mockito.mock(Worklog.class);
+		Mockito.when(worklog.getDuration()).thenReturn(8);
+		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(), ArgumentMatchers.any(LocalDate.class))).thenReturn(Arrays.asList(worklog));
+				
+		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
+		
+		assertEquals(DayStatus.RIGHT_HOURS, resultado.getStatusList().get(0));
+		
+	}
+	
+	@Test
+	public void if_the_worklog_for_the_resquested_day_is_more_than_8_hours_the_status_is_EXTRA_HOURS() {
+		Worklog worklog = Mockito.mock(Worklog.class);
+		Mockito.when(worklog.getDuration()).thenReturn(10);
+		Mockito.when(worklogRepository.findByUsernameAndDate(ArgumentMatchers.anyString(), ArgumentMatchers.any(LocalDate.class))).thenReturn(Arrays.asList(worklog));
+				
+		DayStatusSummary resultado = reportsService.getDayStatusSummaryForWorkerAndDay("USU", LocalDate.now());
+		
+		assertEquals(DayStatus.EXTRA_HOURS, resultado.getStatusList().get(0));
+		
+	}
+	
+
 }
